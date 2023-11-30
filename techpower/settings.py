5c6b6adf8d71a70868cf86cb218aba2247cfe13d
Michelle Totherow <tym4me13@gmail.com>
2023-11-30 07:14:37 -0600

    update to CORS

--- techpower/settings.py
+++ techpower/settings.py
@@ -56,7 +56,8 @@ REST_FRAMEWORK = {
 # THIS IS NEW
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
-    'http://127.0.0.1:3000'
+    'http://127.0.0.1:3000',
+    'http://localhost:5173',
 )
 
 
